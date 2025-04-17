33e617e85415afa78f511bf5471594537a139854
Arnav Deep <73274154+arnavsinghRajput@users.noreply.github.com>
2025-04-17 11:04:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -42,9 +42,30 @@ resource "aws_security_group" "eks_sg" {
   }
 }
 
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "eks.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+
 resource "aws_eks_cluster" "eks" {
   name     = var.cluster_name
-  role_arn = var.admin_role_arn
+  role_arn = aws_iam_role.eks_cluster_role.arn
 
   vpc_config {
     subnet_ids              = var.subnet_ids
@@ -138,3 +159,4 @@ output "cluster_endpoint" {
 output "kubeconfig_command" {
   value = "aws eks update-kubeconfig --region ap-south-1 --name ${var.cluster_name}"
 }
+
